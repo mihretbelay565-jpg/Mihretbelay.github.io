0302c71190b8d6cc62cdfd9946f232af4935d814
mihretbelay565-jpg <mihretbelay565@gmail.com>
2026-05-29 02:06:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'mihret-portfolio-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
